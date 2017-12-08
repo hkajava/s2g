47e1cbdb61876fa7d735c6fd7b093ef29dbff434
Henri Kajava <henri.kajava@gmail.com>
2017-12-08 09:45:51 +0200

    remove obsolete commented out code.

--- imports/ui/StudentGroup.jsx
+++ imports/ui/StudentGroup.jsx
@@ -1,23 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
-// import classnames from 'classnames';
 
-// StudentGroup component - represents a single todo item
 export default class StudentGroup extends Component {
   constructor(props) {
     super(props);
 
-    this.deleteThisStudentGroup = this.deleteThisStudentGroup.bind(this);
     this.selectThisStudentGroup = this.selectThisStudentGroup.bind(this);
     this.editThisStudentGroup = this.editThisStudentGroup.bind(this);
-    // this.togglePrivate = this.togglePrivate.bind(this);
+    this.deleteThisStudentGroup = this.deleteThisStudentGroup.bind(this);
   }
 
-
   selectThisStudentGroup() {
-    // Set the checked property to the opposite of its current value
-    // Meteor.call('studentGroups.setChecked', this.props.studentGroup._id,
-    // !this.props.studentGroup.checked);
     this.props.cbSelect(this.props.studentGroupID, this.props.studentGroupName);
   }
   editThisStudentGroup() {
@@ -48,13 +41,8 @@ export default class StudentGroup extends Component {
 }
 
 StudentGroup.propTypes = {
-
-  // This component gets the studentGroup to display through a React prop.
-  // We can use propTypes to indicate it is required
-  // key: PropTypes.string.isRequired,
   studentGroupID: PropTypes.string.isRequired,
   studentGroupName: PropTypes.string.isRequired,
   cbSelect: PropTypes.func.isRequired,
   cbEdit: PropTypes.func.isRequired,
-  // showPrivateButton: PropTypes.bool.isRequired,
 };
