37427e54c49c10cb0be717029b5a733d19a67955
Henri Kajava <henri.kajava@gmail.com>
2018-01-01 18:37:00 +0200

    comment out debugger command

--- imports/ui/RandomizeStudentGroup.jsx
+++ imports/ui/RandomizeStudentGroup.jsx
@@ -45,7 +45,7 @@ export default class RandomizeStudentGroup extends Component {
 
   // New algorithm
   static findNbrOfSmallGroups(nbrPresentStudents, minGroupSize) {
-    debugger;
+    // debugger;
     if (nbrPresentStudents <= 2 * minGroupSize) {
       // there is not enough present students to divide them into small groups.
       return 1;
